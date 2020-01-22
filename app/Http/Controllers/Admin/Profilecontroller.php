827ed9631e251c566037d5d70bcf639612948bd1
yukishiozaki <manten11@gmail.com>
2020-01-22 23:48:11 +0900

    first commit

---
+++ app/Http/Controllers/Admin/Profilecontroller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Profilecontroller extends Controller
+{
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
+
+  public function edit()
+  {
+      return view('admin.profile.edit');
+  }
+
+  public function update()
+  {
+      return redirect('admin/profile/edit');
+  }
+}
