23a81eb8dad2148301103da92d136f0bc8deca4d
yukishiozaki <manten11@gmail.com>
2020-01-23 00:00:25 +0900

    third commit

--- app/Http/Controllers/Admin/Profilecontroller.php
+++ app/Http/Controllers/Admin/Profilecontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// コントローラー
+
 class Profilecontroller extends Controller
 {
   public function add()
